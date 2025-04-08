c7c9a20058140382061384bf9a661df3cd7b9f83
Leo Dadi <leo.dadi@student-cs.fr>
2025-04-08 18:14:06 +0200

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await auth();
+
   return (
-      <p>Hello World</p>
-  )
+    <div>
+      {session ? (
+        <div>
+          <p>Welcome, {session.user?.name}</p>
+          <p>Email: {session.user?.email}</p>
+          <p>Role: {session.user?.role}</p>
+          {/* Only display necessary session information */}
+        </div>
+      ) : (
+        <p>Not signed in</p>
+      )}
+    </div>
+  );
 }
